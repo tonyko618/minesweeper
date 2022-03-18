5641220106d1b0eeb9ac8967c0454f8745cd7c09
tonyko618 <101879620+tonyko618@users.noreply.github.com>
2022-03-18 16:25:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tkinter as tk
 
 class MainWindow:
     def __init__(self, master):
-        self.master = master
+        self.master=master
         self.mainFrame = tk.Frame(master=self.master)
         self.mainFrame.pack()
 
@@ -44,32 +44,32 @@ class GameFrame:
         self.width = width
         self.height = height
         self.BombNum = BombNum
-        self.__BombLeft = self.BombNum
-        self.__BombArray = [False]*(width*height)
+        self.BombLeft = self.BombNum
+        self.BombArray = [False]*(width*height)
         
 
-        for i in range(len(self.__BombArray)):
-            if self.__BombLeft/(len(self.__BombArray)-i) >= rn.random():
-                self.__BombArray[i] = True
-                self.__BombLeft -= 1
+        for i in range(len(self.BombArray)):
+            if self.BombLeft/(len(self.BombArray)-i) >= rn.random():
+                self.BombArray[i] = True
+                self.BombLeft -= 1
 
-        self.__TFGrid = np.array(self.__BombArray).reshape(self.height, self.width)
-        print(self.__TFGrid)
+        self.TFGrid = np.array(self.BombArray).reshape(self.height, self.width)
+        print(self.TFGrid)
 
-        self.__CellGrid = []
+        self.CellGrid = []
         for i in range(self.height):
             self.CellArray = []
             self.frame = tk.Frame(master=self.mainFrame)
             self.frame.pack()
             for j in range(self.width):
                 self.CellArray.append(Cell(master=self.frame, grid=self.get, x=j, y=i, width=self.width, height=self.height))
-            self.__CellGrid.append(self.CellArray)
+            self.CellGrid.append(self.CellArray)
         
 
 
     def get(self, x, y):
         try:
-            return self.__CellGrid[y][x]
+            return self.CellGrid[y][x]
         except:
             class CellNotFound(exception):
                 pass
@@ -93,6 +93,13 @@ class Cell:
         self.button.bind("<Button-3>", self.RightClick)
         self.button.place(x=0, y=0, width=30, height=30)
 
+    def neighbour(self):
+        output = []
+        for x in range(self.x-1, self.x+2):
+            for y in range(self.y-1, self.y+2):
+                if x>=0 and y>=0 and y<self.parent.height and x<self.parent.width:
+                    output.append((x,y))
+        return output
 
     
     def LeftClick(self, event=0):
@@ -106,20 +113,14 @@ class Cell:
         else:
             self.button["bg"] = "green"
             self.adj = 0
-            for x in range(self.x-1, self.x+2):
-                for y in range(self.y-1, self.y+2):
-                    if x<0 or y<0 or y>=self.parent.height or x>=self.parent.width:
-                        continue
-                    if self.parent.get(x,y).bomb:
-                        self.adj += 1
+            for (x, y) in self.neighbour():
+                if self.parent.get(x,y).bomb:
+                    self.adj += 1
   
             self.button["text"] = str(self.adj)
             if self.adj == 0:
-                for x in range(self.x-1, self.x+2):
-                    for y in range(self.y-1, self.y+2):
-                        if x<0 or y<0 or y>=len(self.grid) or x>=len(self.grid[0]):
-                            continue
-                        self.parent.get(x,y).LeftClick()  
+                for (x, y) in self.neighbour():
+                    self.parent.get(x,y).LeftClick()  
 
     def RightClick(self, event):
         if self.revealed:
