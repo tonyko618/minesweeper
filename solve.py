01c59a38cd593241c864417dbe049dcb9868f7b7
tonyko618 <101879620+tonyko618@users.noreply.github.com>
2022-05-17 16:11:32 +0100

    update

--- solve.py
+++ solve.py
@@ -1,3 +1,13 @@
+#the function solve takes in 4 arguments
+#the total number of bombs in the grid (integer)
+#revealed cells (array of boolean)
+#cells that are marked to be bombs by the player (array of boolean)
+#the number of bombs adjacent to a cell (array of integer) (the value would be None for cells that are not revealed yet)
+#it should return an array of the same dimension, with integer values from 0 to 255
+#0 indicates a cell have 0 probability to be a bomb
+#255 indicates a cell is definitely a bomb
+
+import numpy as np
 def solve(TotalBombBUm, revealed, flagged, adj):
     print("-------------------------------------------")
     print("revealed:\n", revealed)
@@ -6,6 +16,16 @@ def solve(TotalBombBUm, revealed, flagged, adj):
     print("TotalBombBUm: ", TotalBombBUm)
 
     width = len(revealed[0])
-    helight = len(revealed)
-    return 0
-    
+    height = len(revealed)
+    output = np.zeros(shape=(height, width), dtype=np.int32)
+
+
+
+
+
+
+
+
+
+    print(output)
+    return output
